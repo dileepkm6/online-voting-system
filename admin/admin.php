40f79ebc604265176eca20df0ee2c2975458810f
dileep <dileepkm6@gmail.com>
2018-03-19 23:22:29 +0530

    admin folder added

---
+++ admin/admin.php
@@ -0,0 +1,84 @@
+
+<?php
+session_start();
+if(!isset($_SESSION['admin_id']))
+{
+  header('location:index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>online voting</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<script language="JavaScript" src="js/user.js">
+</script>
+
+</head>
+<body id="top">
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <div id="logo" class="fl_left">
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="admin.php">Home</a></li>
+        <li><a class="drop" href="#">Admin HomePages</a>
+          <ul>
+            <li><a href="create_election.php">New Election</a></li>
+            <li><a href="candidates.php">Add Candidate</a></li>
+            <li><a href="refresh.php">View Result</a></li>
+          </ul>
+        </li>
+        
+        <li><a href="http://localhost/SE_Project/index.php">Voter Panel</a></li>
+        <li><a href="logout.php">Logout</a></li>
+
+      </ul>
+    </nav>
+  </header>
+</div>
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/b1.jpg');">
+  <section id="testimonials" class="hoc container clear"> 
+   
+    <h2 class="font-x3 uppercase btmspace-80 "><a href="#">Online Voting System</a></h2>
+    <ul class="nospace group">
+
+       <li class="one_third">
+
+          <blockquote>This is Admin homepage, Admin can set candidates for voting and view results.</blockquote>
+        
+      </li> 
+
+    </ul>
+   
+  </section>
+</div>
+
+
+
+  </footer>
+</div>
+
+<div class="wrapper row6">
+  <div> 
+    <p>Dileep Kr Maurya</p>
+    <p>NITK Surathkal</p>
+    <p>Email:<a href="https://gmail.com">dileepkm6@gmail.com</a></p>
+  </div>
+</div>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+</body>
+</html>
