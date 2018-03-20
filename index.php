8bbb43d6248a4022a6de3022c84ac396b7db444d
dileep <dileepkm6@gmail.com>
2018-03-20 12:00:43 +0530

    index file added

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>online voting</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<!-- <link href="css/user_styles.css" rel="stylesheet" type="text/css" /> -->
+<script language="JavaScript" src="js/user.js">
+</script>
+
+</head>
+<body id="top">
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a class="drop" href="#">Pages</a>
+          <ul>
+            <li><a href="admin/index.php">Admin</a></li>
+            <li><a href="login.php">Voter</a></li>
+            
+          </ul>
+        </li>
+        <li><a class="drop" href="#">Voter Panel</a>
+          <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registeracc.php">Registration</a></li>
+            
+          </ul>
+        </li>
+       
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/b4.jpeg');">
+  <section id="testimonials" class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <h2 class="font-x3 uppercase btmspace-80"><a href="#">Online Voting System</a></h2>
+    <ul class="nospace group">
+      <li class="one_half">
+        <blockquote><img src="images/demo/backgrounds/background2"></blockquote>
+      
+      </li>
+    </ul>
+  </section>
+</div>
+<div class="wrapper row6">
+  <div> 
+    <p>Dileep Kr Maurya</p>
+    <p>NITK Surathkal</p>
+    <p>Email:<a href="https://gmail.com">dileepkm6@gmail.com</a></p>
+  </div>
+</div>
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+</body>
+</html>
+
+
